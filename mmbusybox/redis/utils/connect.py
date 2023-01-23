9d150e39310f79a357becdc45c0b35b5cdcc29b1
sth-v <aa@contextmachine.online>
2023-01-23 20:10:52 +0300

    many
    Signed-off-by: sth-v <aa@contextmachine.online>

--- mmbusybox/redis/utils/connect.py
+++ mmbusybox/redis/utils/connect.py
@@ -11,13 +11,13 @@ def bootstrap_local(url=os.getenv("REDIS_URL"), db=0) -> redis.Redis | redis.Str
     return conn
 
 
-def bootstrap_cloud_small(host=s.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"),
+def bootstrap_cloud_small(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"),
                           password=os.getenv("REDIS_PASSWORD"), db=0) -> redis.Redis | redis.StrictRedis | Any:
     os.environ["REDIS_URL"], os.environ["REDIS_DB"] = host + ":" + port, db
     r = redis.StrictRedis(
         host="c-c9q1muil9vsf3ol4p3di.rw.mdb.yandexcloud.net",
         port=6379,
         password=password,
-    )
+        )
 
     return r
